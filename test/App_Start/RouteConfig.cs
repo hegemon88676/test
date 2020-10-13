7c6d3a518e0be53b24fae0b4e79d4224b6a964c8
hegemon88676 <ff_cris2000@yahoo.com>
2020-10-13 16:58:27 +0300

    cv stupid

--- test/App_Start/RouteConfig.cs
+++ test/App_Start/RouteConfig.cs
@@ -14,8 +14,8 @@ namespace test
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
+            
+                url: "{controller}/{action}/{id}",    name: "Default",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
